d1bc748faed0eccf6c0b800dd00b18b743c4feaa
haleyraburn <raburnhaley@ou.edu>
2024-10-16 13:14:44 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('24.248.253.76', 'haleyraburn', 'OUprincess56', 'mis4013haleyraburn3');
+    $conn = new mysqli('138.197.17.168', 'misoucr3_hw3user', 'pXcbd0ZC)8kK', 'misoucr3_mis4013hw3');
     
     // Check connection
     if ($conn->connect_error) {
