03912182bd3978a519237b9a148fe167b57a9bb4
haleyraburn <raburnhaley@ou.edu>
2024-10-22 13:00:31 -0500

    Update and rename model-instructors.php to model-swimmers.php

--- model-swimmers.php
+++ model-swimmers.php
@@ -1,8 +1,8 @@
 <?php
-function selectInstructors() {
+function selectSwimmers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT instructor_id, instructor_name, office_number FROM instructor");
+        $stmt = $conn->prepare("SELECT swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM swimmer");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
