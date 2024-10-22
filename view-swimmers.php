d0fbd0644cb85bc05820b2d686af5334bd0845d4
haleyraburn <raburnhaley@ou.edu>
2024-10-22 13:46:03 -0500

    Update view-swimmers.php

--- view-swimmers.php
+++ view-swimmers.php
@@ -12,9 +12,12 @@
   </thead>
   <tbody>
 <?php
-$conn = new mysqli('hostname', 'username', 'password', 'database');
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+function get_db_connection(){
+    $conn = new mysqli('138.197.17.168', 'misoucr3_hw3user', 'pXcbd0ZC)8kK', 'misoucr3_mis4013hw3');
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
 }
 $query = "SELECT swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM swimmers";
 $swimmers = $conn->query($query);
