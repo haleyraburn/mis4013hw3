0eca48d3a3c0264217275ee31b33ff98a91f6145
haleyraburn <raburnhaley@ou.edu>
2024-10-22 15:48:37 -0500

    Update view-meets.php

--- view-meets.php
+++ view-meets.php
@@ -3,9 +3,9 @@
 <h1>Meets</h1>
   </div>
   <div class="col auto">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-    </svg>
+<>php
+  include "view-meets-newform.php";
+?>
   </div>
 </div>
 <div class="table-responsive">
