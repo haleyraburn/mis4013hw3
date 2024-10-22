dfe45236a478204e94a29cf8d2bee9f989e1c036
haleyraburn <raburnhaley@ou.edu>
2024-10-22 13:03:19 -0500

    Update and rename sections-by-course.php to events-by-meet.php

---
+++ events-by-meet.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-events-by-meet.php");
+
+$pageTitle = "Events by Meet";
+include "view-header.php";
+$selections = selectEventsByMeet($_POST['cid']);
+include "view-events-by-meet.php";
+include "view-footer.php";
+?>
