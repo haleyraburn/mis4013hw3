bec39352801749ed68a127c7b56ca0b3973736e0
haleyraburn <raburnhaley@ou.edu>
2024-10-21 22:17:18 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -2,7 +2,7 @@
 function selectCourses() {
   try {
     $conn = get_db_connection();
-  $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM 'course'");
+  $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM course");
   $stmt->execute();
   $result = $stmt->get_result();
   $conn->close();
