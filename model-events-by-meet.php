6e82bcb9451864e04f24a7c979efa83122975962
haleyraburn <raburnhaley@ou.edu>
2024-10-22 13:33:38 -0500

    Update model-events-by-meet.php

--- model-events-by-meet.php
+++ model-events-by-meet.php
@@ -3,7 +3,7 @@ function selectEventsByMeet($cid) {
   try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT m.meet_id, meet_name, meet_location, meet_daytime FROM meet m join event e on e.meet_id = m.meet_id where e.meet_id=?");
-        $stmt->bind_param("i", $cid);
+        $stmt->bind_param("i", $mid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
