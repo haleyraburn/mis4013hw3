860230d116ee3517f5e613ab1c6aa9f6f8fac25c
haleyraburn <raburnhaley@ou.edu>
2024-10-22 16:15:16 -0500

    Update events-by-meet.php

--- events-by-meet.php
+++ events-by-meet.php
@@ -4,7 +4,7 @@ require_once("model-events-by-meet.php");
 
 $pageTitle = "Events by Meet";
 include "view-header.php";
-$selections = selectEventsByMeet($_POST['cid']);
+$selections = selectEventsByMeet($_POST['mid']);
 include "view-events-by-meet.php";
 include "view-footer.php";
 ?>
