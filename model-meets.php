49bb198da3ea494ee0d109f197d524e5545db839
haleyraburn <raburnhaley@ou.edu>
2024-10-22 12:57:12 -0500

    Update and rename model-courses.php to model-meets.php

--- model-meets.php
+++ model-meets.php
@@ -1,8 +1,8 @@
 <?php
-function selectCourses() {
+function selectMeets() {
   try {
     $conn = get_db_connection();
-  $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM course");
+  $stmt = $conn->prepare("SELECT meet_id, meet_name, meet_location, meet_daytime FROM meet");
   $stmt->execute();
   $result = $stmt->get_result();
   $conn->close();
