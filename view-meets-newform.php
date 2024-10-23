f2011ab23ec9ff1d25d66fd3cbc0a446974ada3a
haleyraburn <raburnhaley@ou.edu>
2024-10-22 20:32:25 -0500

    Update view-meets-newform.php

--- view-meets-newform.php
+++ view-meets-newform.php
@@ -1,15 +1,15 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#New Meet">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newMeetModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
     </svg>
 </button>
 <!-- Modal -->
-<div class="modal fade" id="New Meet" tabindex="-1" aria-labelledby="newMeetModalLabel" aria-hidden="true">
+<div class="modal fade" id="newMeetModal" tabindex="-1" aria-labelledby="newMeetModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newMeetModalLabel">New meet</h1>
+        <h1 class="modal-title fs-5" id="newMeetModalLabel">New Meet</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
