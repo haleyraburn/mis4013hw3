473f9dd9428756cdd9aa8dee471b60a14c384637
haleyraburn <raburnhaley@ou.edu>
2024-10-22 20:54:32 -0500

    Update meets.php

--- meets.php
+++ meets.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertMeet($_POST['mName'], $_POST['mLocation'], $_POST['mDayTime']);
+      if (insertMeet($_POST['mName'], $_POST['mLocation'], $_POST['mDayTime'])) {
+        echo '<div class="alert alert-success" role="alert"> Meet added. </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error. </div>';
+      }
     break;
   }
 }
