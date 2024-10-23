0a513a5935bc2317eb4a4b58024c66d6d023c371
haleyraburn <raburnhaley@ou.edu>
2024-10-22 20:39:45 -0500

    Update meets.php

--- meets.php
+++ meets.php
@@ -4,6 +4,15 @@ require_once("model-meets.php");
 
 $pageTitle = "Meets";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertMeet($_POST['mName'], $_POST['mLocation'], $_POST['mDayTime']);
+    break;
+  }
+}
+
 $meets = selectMeets();
 include "view-meets.php";
 include "view-footer.php";
