a12f1e4edd7b9d765d13f2d0de48c8c3d675f7bc
haleyraburn <raburnhaley@ou.edu>
2024-10-22 21:18:00 -0500

    Update meets.php

--- meets.php
+++ meets.php
@@ -15,7 +15,7 @@ if (isset($_POST['actionType'])) {
       }
     break;
         case "Delete":
-      if (deleteMeet($_POST['mName'])) {
+      if (deletedMeet($_POST['mName'])) {
         echo '<div class="alert alert-success" role="alert"> Meet deleted. </div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error. </div>';
